b4ac5e0f42857057c8233c22bce61434083ee088
RackGamesDev <yesterdiscord7@gmail.com>
2025-05-22 11:10:04 +0200

    Avances

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # ScriptingReferenceTypescript
 Lo básico de Typescript (solo lo distinto de Javascript) en español
 Para abrirlo: 
-
+`sudo npm i -g typescript`
+`tsc index.ts`
+Typescript no se ejecuta directamente, se compila a Javascript
+Crear el tsconfig.json para la configuración de compilación: `tsc --init`
 
 ## License
 This project is licensed under the GNU General Public License v3.0.  
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+//Typescript es igual que Javascript pero con algunos cambios como que las variables estan estáticamente tipadas, aquí se veran todas las diferencias en cuanto a Javascript
+//Typescript usa nodejs para compilar los archivos .ts a .js (no se puede ejecutar un archivo .ts directamente) (comando tsc)
+console.log("hola");
+
+//Tipos:
+let numero:number = 10;
+//numero = "hola"; //Imposible
